41959d737f95ff88aeac89d3b35869a1e1d95a8a
SeeunChoi1 <candy970106@gmail.com>
2023-01-05 00:11:26 +0900

    [solved] 배열돌리기3

--- exercise/baekjoon/구현/배열돌리기3.py
+++ exercise/baekjoon/구현/배열돌리기3.py
@@ -5,9 +5,10 @@ sys.stdin = open("input.txt", "r")
 
 row_num, col_num, exe_num = map(int, input().split())
 array = [list(map(int, input().split())) for _ in range(row_num)]
-print(*array, sep="\n")
+# print(*array, sep="\n")
 exe_list = list(map(int, input().split()))
 # print(exe_list)
+
 
 # 1번 연산 - 상하반전
 def exe1(row_num, col_num, array):
@@ -38,7 +39,7 @@ def exe2(row_num, col_num, array):
 # 3번 연산 - 오른쪽 90도 회전
 def exe3(row_num, col_num, array):
     new_array = [[0] * row_num for _ in range(col_num)]
-    print(*new_array, sep="\n")
+    # print(*new_array, sep="\n")
     # row_num <-> col_num
     for i in range(row_num):
         for j in range(col_num):
@@ -60,12 +61,42 @@ def exe4(row_num, col_num, array):
 
 # 5번 연산 - 부분배열 1->2 / 2->3 / 3->4 / 4->1
 def exe5(row_num, col_num, array):
-    pass
+    new_array = [[0] * col_num for _ in range(row_num)]
+    row_part = row_num // 2
+    col_part = col_num // 2
+
+    for i in range(row_num):
+        for j in range(col_num):
+            if i < row_part and j < col_part:
+                new_array[i][j + col_part] = array[i][j]
+            elif i < row_part and j >= col_part:
+                new_array[i + row_part][j] = array[i][j]
+            elif i >= row_part and j >= col_part:
+                new_array[i][j - col_part] = array[i][j]
+            elif i >= row_part and j < col_part:
+                new_array[i - row_part][j] = array[i][j]
+
+    return new_array
 
 
 # 6번 연산 - 부분배열 1->4 / 4->3 / 3->2 / 2->1
 def exe6(row_num, col_num, array):
-    pass
+    new_array = [[0] * col_num for _ in range(row_num)]
+    row_part = row_num // 2
+    col_part = col_num // 2
+
+    for i in range(row_num):
+        for j in range(col_num):
+            if i < row_part and j < col_part:
+                new_array[i + row_part][j] = array[i][j]
+            elif i < row_part and j >= col_part:
+                new_array[i][j - col_part] = array[i][j]
+            elif i >= row_part and j >= col_part:
+                new_array[i - row_part][j] = array[i][j]
+            elif i >= row_part and j < col_part:
+                new_array[i][j + col_part] = array[i][j]
+
+    return new_array
 
 
 for exe in exe_list:
@@ -75,12 +106,16 @@ for exe in exe_list:
         new_array = exe2(row_num, col_num, array)
     elif exe == 3:
         new_array = exe3(row_num, col_num, array)
+        row_num, col_num = col_num, row_num
     elif exe == 4:
         new_array = exe4(row_num, col_num, array)
+        row_num, col_num = col_num, row_num
     elif exe == 5:
         new_array = exe5(row_num, col_num, array)
     elif exe == 6:
         new_array = exe6(row_num, col_num, array)
+    array = new_array
 
-print(">>>>>>>")
-print(*new_array, sep="\n")
+# print(*array, sep="\n")
+for a in array:
+    print(*a, sep=" ")
