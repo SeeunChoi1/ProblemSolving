2b9c2d3b47077301445ad001cdf7cb224282b70c
SeeunChoi1 <candy970106@gmail.com>
2024-06-19 21:25:40 +0900

    [solved] leetcode75 - 345

---
+++ exercise/leetcode/reverse-vowels-of-a-string.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def reverseVowels(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        vowels = ['a', 'e', 'i', 'o', 'u']
+        s_list = list(s)
+        queue = []
+        for elem in s:
+            if elem.lower() in vowels:
+                queue.append(elem)
+        for i in range(len(s_list)):
+            if s_list[i].lower() in vowels:
+                s_list[i] = queue[-1]
+                queue.pop(-1)
+
+        return "".join(s_list)
+
+
