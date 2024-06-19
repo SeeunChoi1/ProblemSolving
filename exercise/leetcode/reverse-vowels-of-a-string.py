d4d591ed23f5f5fd8b61cd872eb786ef4102ca0f
SeeunChoi1 <candy970106@gmail.com>
2024-06-19 21:33:08 +0900

    [solved] leetcode75 - 345 (ver2)

--- exercise/leetcode/reverse-vowels-of-a-string.py
+++ exercise/leetcode/reverse-vowels-of-a-string.py
@@ -7,14 +7,23 @@ class Solution(object):
         vowels = ['a', 'e', 'i', 'o', 'u']
         s_list = list(s)
         queue = []
+        # for elem in s:
+        #     if elem.lower() in vowels:
+        #         queue.append(elem)
+        # for i in range(len(s_list)):
+        #     if s_list[i].lower() in vowels:
+        #         s_list[i] = queue[-1]
+        #         queue.pop(-1)
+        # return "".join(s_list)
+
         for elem in s:
             if elem.lower() in vowels:
                 queue.append(elem)
+        pointer = len(queue) - 1
         for i in range(len(s_list)):
             if s_list[i].lower() in vowels:
-                s_list[i] = queue[-1]
-                queue.pop(-1)
-
+                s_list[i] = queue[pointer]
+                pointer -= 1
         return "".join(s_list)
 
 
