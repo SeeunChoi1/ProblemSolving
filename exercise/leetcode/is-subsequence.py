49044b5943a6c08072063ea40cffc0664ec5f7ae
SeeunChoi1 <candy970106@gmail.com>
2024-06-21 17:51:36 +0900

    [solved] leetcode75 - 392

---
+++ exercise/leetcode/is-subsequence.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def isSubsequence(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        index = 0
+        # t 순회
+        # s[index] 있으면 넘어감
+        # index랑 s길이 비교
+
+        if not s:
+            return True
+
+        if t:
+            for elem in t:
+                if elem == s[index]:
+                    index += 1
+                if index == len(s):
+                    return True
+        else:
+            return False
